215195e6389dbe2eb79db0f92f1be6d87b766f9e
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2021-09-02 22:22:24 -0700

    'ok'

--- app/Http/Controllers/API/generalController.php
+++ app/Http/Controllers/API/generalController.php
@@ -13,6 +13,7 @@ class generalController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth:api');
 //        $this->middleware('auth:api')->except(['tournaments','appupdateinfo','paymentmethods']);
     }
 public function updateProfile(Request $request)
