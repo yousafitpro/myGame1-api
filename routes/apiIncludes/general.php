78491b184631459d875184616479fe22a9bfc600
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2021-09-05 08:28:15 -0700

    'ok'

--- routes/apiIncludes/general.php
+++ routes/apiIncludes/general.php
@@ -6,7 +6,7 @@ Route::group([
 
 
     Route::post('updateProfile', 'generalController@updateProfile');
-    Route::get('get_web_config', 'generalController@get_web_config');
+//    Route::get('get_web_config', 'generalController@get_web_config');
 });
 Route::group([
     'namespace' => 'App\Http\Controllers\API',
